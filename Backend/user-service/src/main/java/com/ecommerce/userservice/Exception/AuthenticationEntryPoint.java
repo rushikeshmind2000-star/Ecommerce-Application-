3b43e5d8b3bb756634ccb53392b2e19bc0028bc5
Rohit <birajdarrohit56@gmail.com>
2026-09-14 12:23:13 +0530

    Update user service with Keycloak security

--- Backend/user-service/src/main/java/com/ecommerce/userservice/Exception/AuthenticationEntryPoint.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.userservice.Exception;
-
-public class AuthenticationEntryPoint extends RuntimeException {
-    public AuthenticationEntryPoint(String message) {
-        super(message);
-    }
-}
--- Backend/user-service/src/main/java/com/ecommerce/userservice/Exception/JwtAuthenticationEntryPoint.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.userservice.Exception;
-
-public class JwtAuthenticationEntryPoint extends RuntimeException {
-    public JwtAuthenticationEntryPoint(String message) {
-        super(message);
-    }
-}
