14de6f6c4d8dbcf6904e8f2668eac9016278fd20
RusheeMind55 <mindrushi55@gmail.com>
2026-09-15 16:33:23 +0530

    communication with product service and user service

---
+++ Backend/user-service/src/main/java/com/ecommerce/userservice/client/ProductClient.java
@@ -0,0 +1,13 @@
+package com.ecommerce.userservice.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.UUID;
+
+@FeignClient(name = "product-service")
+public interface ProductClient {
+
+    @GetMapping("/api/products/{id}")
+    Object getProduct(@PathVariable("id") UUID id);
+}
